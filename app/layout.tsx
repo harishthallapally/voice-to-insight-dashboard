76cd077dd18850b5cf4524df827e1ca16c1e1a5c
Harish Thallapalli <harishthallapally@gmail.com>
2026-06-13 19:25:03 +0530

    updated description

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "AI voice to insights dashboard",
-  description: "AI voice to insights dashboard"
+  description: "AI voice to insights dashboard."
 };
 
 export default function RootLayout({
